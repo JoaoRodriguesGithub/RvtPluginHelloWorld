ec0420aff22a2d7252b9656678b96355a60239fd
JR-LAPTOP01\JR <a20629@alunos.ipca.pt>
2023-06-25 11:32:14 +0100

    feat: Add Hello World Plugin

---
+++ src/RvtPluginHelloWorld/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RvtPluginHelloWorld
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData revit,
+           ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Result.Succeeded;
+        }
+    }
+}
