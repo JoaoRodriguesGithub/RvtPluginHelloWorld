8c9a958a37886435b8dac0bb50700e4e07c57241
JR-LAPTOP01\JR <a20629@alunos.ipca.pt>
2023-07-23 15:37:19 +0100

    feat: add lab1 and lab2 plugins

---
+++ src/Lab5PlaceGroup/GroupPickFilter.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace Lab5PlaceGroup
+{
+    // Filter to constrain picking to model groups. Only model groups
+    // are highlighted and can be selected when cursor is hovering.
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element e)
+        {
+            return (e.Category.Id.IntegerValue.Equals((int)BuiltInCategory.OST_IOSModelGroups));
+        }
+        public bool AllowReference(Reference r, XYZ p)
+        {
+            return false;
+        }
+    }
+}
---
+++ src/Lab5PlaceGroup/Lab5PickFilter.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+
+namespace Lab5PlaceGroup
+{
+    public class Lab5PickFilter : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            try
+            {
+                //Define a reference Object to accept the pick result
+                Reference pickedRef = null;
+
+                //Pick a group
+                Selection sel = uiapp.ActiveUIDocument.Selection;
+
+                GroupPickFilter selFilter = new GroupPickFilter();
+                pickedRef = sel.PickObject(ObjectType.Element, selFilter, "Please select a group");
+                
+                Element elem = doc.GetElement(pickedRef);
+                Group group = elem as Group;
+
+                //Pick point
+                XYZ point = sel.PickPoint("Please pick a point to place group");
+
+                //Place the group
+                Transaction trans = new Transaction(doc);
+                trans.Start("Lab");
+                doc.Create.PlaceGroup(point, group.GroupType);
+                trans.Commit();
+            }
+
+            //If the user right-clicks or presses Esc, handle the exception
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
+
+            return Result.Succeeded;
+        }
+    }
+}
